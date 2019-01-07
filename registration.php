3c1be20f2aaa78e13451521b235d6948e21584ac
William French <will@jamersan.com>
2019-01-07 15:22:34 -0600

    Add composer.json and module registration files

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * Copyright (c) 2019. Jamersan, LLC
+ * Author: William French
+ * https://www.jamersan.com | william.french@jamersan.com
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Jamersan_GoogleTagManager',
+    __DIR__
+);
